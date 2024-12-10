d714a1cff392b4ae1eb6c2a7143b9334e424bb1a
Catuzzi <130846749+Catuzzi@users.noreply.github.com>
2024-12-09 21:53:50 -0300

    Update forms.py
    Forms documentado

--- loja_virtual_app/forms.py
+++ loja_virtual_app/forms.py
@@ -1,14 +1,16 @@
 from django import forms
 from .models import Produto
 
-class ProdutoForm(forms.ModelForm):
+# Formulário para cadastrar o produto
+class ProdutoForm(forms.ModelForm): # A classe forms.ModelForm é uma subclasse do Django Forms que cria um formulário automaticamente baseado em um modelo
     class Meta:
         model = Produto
         fields = ['nome', 'descricao', 'preco', 'quantidade', 'imagem']
-        widgets = {
+        widgets = { # Personaliza a aparência e o comportamento de cada campo no formulário
             'nome': forms.TextInput(attrs={'class': 'form-control'}),
-            'descricao': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+            'descricao': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}), # Define a altura inicial do campo em 5 linhas
             'preco': forms.NumberInput(attrs={'class': 'form-control'}),
             'quantidade': forms.NumberInput(attrs={'class': 'form-control'}),
             'imagem': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
         }
+
